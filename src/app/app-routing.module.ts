732bd2b3845e9756c9d3a8f5777effee936dd7c1
MudiyalaSivareddy <89572838+MudiyalaSivareddy@users.noreply.github.com>
2021-11-09 15:58:16 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { FetilizerPageComponent } from './fetilizer-page/fetilizer-page.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'search/:searchTerm', component:HomeComponent},
+  {path:'tag/:tag', component:HomeComponent},
+  {path:'fetilizers/:id', component:FetilizerPageComponent},
+  {path:'cart-page', component:CartPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
